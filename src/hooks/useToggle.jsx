69c05bf6ae18f13f943a8774c2dbac26e98ef71f
Julien Lisita <julien.lisita@gmail.com>
2025-05-05 23:27:23 +0200

    feat: add custom hook useToggle for boolean state management

---
+++ src/hooks/useToggle.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+export default function useToggle(initial=false)
+{
+    const [state, setState] = useState(initial)
+
+    const toggleState = () => {
+        setState(prev => !prev)
+    }
+
+    return {state, toggleState}
+}
