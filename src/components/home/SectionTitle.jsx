7199b2ac3b4c8bab4cbca72aae1a96e64ea5d58c
Julien Lisita <julien.lisita@gmail.com>
2025-05-06 23:58:49 +0200

    feat: create reusable SectionTitle component

---
+++ src/components/home/SectionTitle.jsx
@@ -0,0 +1,7 @@
+export default function SectionTitle({ children }) {
+    return (
+      <h2 className="mt-8 mb-14 text-lg sm:text-xl lg:text-3xl font-semibold text-white">
+        {children}
+      </h2>
+    );
+  }
