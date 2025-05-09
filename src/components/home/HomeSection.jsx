bfcff8bc5b74d6d182e8a36bb137ea41ef1b76f4
Julien Lisita <julien.lisita@gmail.com>
2025-05-09 04:15:26 +0200

    style: correct layout spacing in HomeSection component

--- src/components/home/HomeSection.jsx
+++ src/components/home/HomeSection.jsx
@@ -4,16 +4,20 @@ import SectionTitle from "./SectionTitle";
 export default function HomeSection({ id, title, children, link }) {
     return (
       <section id={id} className="bg-[#222222]">
-        <div className="flex flex-col items-center pt-8 pb-16 px-3">
+        <div className="
+          flex flex-col items-center 
+          gap-6 m:gap-8 lg:gap-12
+          pt-8 pb-8 sm:pt-10 sm:pb-10 lg:pt-14 lg:pb-14
+          px-3 sm:px-5 lg:px-6">
           {/* Titre */}
           <SectionTitle>{title}</SectionTitle>
   
           {/* Contenu principal */}
-          <div className="w-full">{children}</div>
+          {children}
   
           {/* Lien */}
           {link && (
-            <div className="mt-6 text-center">
+            <div className="flex justify-center items-center my-4 sm:my-5 lg:my-6">
               <LinkButton to={link.href}>{link.text}</LinkButton>
             </div>
           )}
