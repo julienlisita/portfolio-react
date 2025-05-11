6ff3b34d37ea23188740f536d3294aa87ed23889
Julien Lisita <julien.lisita@gmail.com>
2025-05-11 11:21:24 +0200

    feat: implement projects section component with skill card component

---
+++ src/components/home/ProjectCard.jsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom";
+
+export default function ProjectCard({ id, image, title, description, technos }) {
+  return (
+    <Link
+      to={`/portfolio/${id}`}
+      className="bg-[#2a2a2a] w-72 h-96 rounded-xl overflow-hidden shadow-[0_0_10px_rgba(155,89,182,0.3),0_0_20px_rgba(0,122,255,0.3)] transition block hover:scale-[1.02] hover:shadow-[0_0_18px_rgba(155,89,182,0.6),0_0_18px_rgba(0,122,255,0.6)] "
+    >
+      <img
+        src={image}
+        alt={title}
+        className="h-48 w-full object-fill"
+      />
+      <div className="p-4">
+        <h3 className="text-lg text-white font-semibold mb-2">{title}</h3>
+        <p className="text-sm text-gray-300 mb-4">{description}</p>
+        <div className="flex flex-wrap gap-2">
+          {technos.map((tech, i) => (
+        <span
+        key={i}
+        className="text-xs bg-[#9B59B6] text-white px-2 py-1 rounded-md"
+        >
+        {tech}
+        </span>
+          ))}
+        </div>
+      </div>
+    </Link>
+  );
+}
--- src/components/home/ProjectsPreview.jsx
+++ src/components/home/ProjectsPreview.jsx
@@ -1,8 +1,47 @@
+import HomeSection from "./HomeSection";
+import ProjectCard from "./ProjectCard";
+
 export default function ProjectPreview()
 {
+
+const projects = [
+  {
+    title: "ShogiConnect",
+    description: "Une plateforme communautaire autour du shogi, avec profils joueurs, forum de discussion",
+    image: "./src/assets/images/projects/shogiconnect.png",
+    technos: ["React", "Node/Express", "Sequelize", "Socket.io"],
+    demo: "#",
+    github: "https://github.com/julienlisita/shogiconnect-client",
+  },
+  {
+    title: "Planify",
+    description: "Une plateforme de création de recettes de cuisine avec planification des repas",
+    image: "./src/assets/images/projects/planify.png",
+    technos: ["React", "Node/Express", "Sequelize", "Taiwind"],
+    demo: "#", 
+    github: "https://github.com/julienlisita/planify-frontend",
+  },
+  {
+    title: "Site Portfolio React",
+    description: "Un site responsive pour un freelance, intégration à partir de maquette Figma.",
+    image: "./src/assets/images/projects/portfolio.png",
+    technos: ["React", "Tailwind", "Frame-Motion"],
+    demo: "#",
+    github: "https://github.com/julienlisita/portfolio-react",
+  },
+];
     return (
-        <>
-            <h2>Derniers projets</h2>
-        </>
+        <HomeSection
+            id="portfolio"
+            title="Derniers projets"
+            link={{href:"/portfolio", text:"Voir le portfolio"}}
+        > 
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-8">
+            {projects.slice(0, 3).map((project) => (
+                <ProjectCard key={project.title} {...project} />
+                ))}
+            </div>
+        </HomeSection>
     )
 }
+
