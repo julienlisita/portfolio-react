7e43e5d7082021c9f4d4ee78b77f483d9b97a073
Julien Lisita <julien.lisita@gmail.com>
2025-05-12 17:23:32 +0200

    feat: create contact form and use it in ContacTeaser section

---
+++ src/components/ContactForm.jsx
@@ -0,0 +1,36 @@
+import Button from "./Button";
+
+export default function ContactForm()
+{
+    return (
+        <form className="w-full max-w-2xl bg-[#2a2a2a] rounded-2xl p-8 shadow-[0_0_12px_#5AC8FA] space-y-6">
+        <div className="flex flex-col">
+          <label className="mb-1 text-sm text-[#FFFFFF]">Nom</label>
+          <input
+            type="text"
+            className="bg-[#1f1f1f] text-white p-3 rounded-md outline-none border border-transparent focus:border-[#5AC8FA] transition"
+            placeholder="Votre nom"
+          />
+        </div>
+        <div className="flex flex-col">
+          <label className="mb-1 text-sm text-[#FFFFFF]">Email</label>
+          <input
+            type="email"
+            className="bg-[#1f1f1f] text-white p-3 rounded-md outline-none border border-transparent focus:border-[#5AC8FA] transition"
+            placeholder="Votre email"
+          />
+        </div>
+        <div className="flex flex-col">
+          <label className="mb-1 text-sm text-[#FFFFFF]">Message</label>
+          <textarea
+            rows="5"
+            className="bg-[#1f1f1f] text-white p-3 rounded-md outline-none border border-transparent focus:border-[#5AC8FA] transition resize-none"
+            placeholder="Votre message"
+          ></textarea>
+        </div>
+        <div className="text-center">
+          <Button type="submit">Envoyer</Button>
+        </div>
+      </form>
+    )
+}
--- src/components/home/ContactTeaser.jsx
+++ src/components/home/ContactTeaser.jsx
@@ -1,8 +1,18 @@
+import ContactForm from "../ContactForm";
+import HomeSection from "./HomeSection";
+
 export default function ContactTeaser()
 {
     return (
-        <>
-            <h2>Envie de discuter d'un projet ?</h2>
-        </>
+        <HomeSection
+            id="contact"
+            title="Me contacter"
+            link={{href:"/contact", text:"Plus d'infos"}}
+        > 
+        <p className="text-white w-4/5 sm:w-4/5 lg:w-2/3">
+            Une idée, un projet ou simplement envie d’échanger ? Remplissez le formulaire ci-dessous et je vous répondrai rapidement.
+        </p>
+        <ContactForm/>
+        </HomeSection>
     )
 }
