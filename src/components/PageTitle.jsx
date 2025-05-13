99e007aa6d4cc8936deb52da8ef4da9db63cb518
Julien Lisita <julien.lisita@gmail.com>
2025-05-13 15:08:33 +0200

    Style: Reduce top margin above page titles

--- src/components/PageTitle.jsx
+++ src/components/PageTitle.jsx
@@ -1,8 +1,8 @@
 export default function PageTitle({ children }) {
     return (
       <h1 className="
-        mt-16 ms:mt-20 lg:mt-32  
-        text-2xl sm:text-4xl lg:text-6xl
+        mt-12 sm:mt-16 lg:mt-24
+        text-2xl sm:text-3xl lg:text-5xl
         font-bold text-white
         text-center">
         {children}
