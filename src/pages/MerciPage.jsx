d9d4330223a9505f8da0b94d12e1d00b2a4d3f8f
Julien Lisita <julien.lisita@gmail.com>
2025-05-14 06:31:04 +0200

    chore: move images to public directory

---
+++ src/pages/MerciPage.jsx
@@ -0,0 +1,10 @@
+import PageTitle from "./PageTitle";
+
+export default function MerciPage() {
+  return (
+    <div className="p-10 text-center text-white">
+      <PageTitle>Merci pour votre message !</PageTitle>
+      <p className="mt-4">Je vous répondrai dans les plus brefs délais.</p>
+    </div>
+  );
+}
