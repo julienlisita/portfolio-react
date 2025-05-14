c85e16f0f279f85a9dc7f1e3f4ac9e9be0cda76c
Julien Lisita <julien.lisita@gmail.com>
2025-05-14 02:29:13 +0200

    feat: implement PortfolioPage

--- src/pages/PortfolioPage.jsx
+++ src/pages/PortfolioPage.jsx
@@ -1,15 +1,28 @@
-import Footer from "../components/Footer";
-import Header from "../components/Header";
+import { projects } from "../data/projectsData";
+import PageTitle from "../components/PageTitle";
+import ProjectCard from "../components/home/ProjectCard";
 
-export default function PortfolioPage() {
-    return (
-      <div>
-        <h1>Mes Projets</h1>
-        <p>Voici mes projets récents :</p>
-        <ul>
-          <li><a href="/portfolio/projet-1">Projet 1</a></li>
-          <li><a href="/portfolio/projet-2">Projet 2</a></li>
-        </ul>
+export default function Portfolio() {
+  return (
+    <div className="pt-14 sm:pt-16 lg:pt-20 text-white pb-16 sm:pb-20 lg:pb-24">
+      {/* Titre de la page */}
+      <PageTitle>Portfolio</PageTitle>
+
+      <div className="mt-8 sm:mt-10 lg:mt-12">
+        {/* Introduction ou section d'accueil */}
+        <div className="text-left">
+          <p className="text-gray-300 mt-4">
+            Voici quelques-uns des projets sur lesquels j'ai travaillé. Cliquez sur chaque projet pour en savoir plus.
+          </p>
+        </div>
+
+        {/* Liste des projets */}
+        <div className="w-full flex flex-wrap justify-center lg:justify-between gap-6 sm:gap-7 lg:gap-8 mt-8 sm:mt-10 lg:mt-12">
+          {projects.slice(0, 3).map((project) => (
+              <ProjectCard key={project.title} {...project} />
+              ))}
+          </div>
+        </div>
       </div>
-    );
-  }
+  );
+}
