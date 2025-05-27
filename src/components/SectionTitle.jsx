58df6056d33762d18a99031b1eca182f54163a84
Julien Lisita <julien.lisita@gmail.com>
2025-05-28 00:08:47 +0200

    fix: adjust SectionTitle anchor offset for sticky header

--- src/components/SectionTitle.jsx
+++ src/components/SectionTitle.jsx
@@ -1,11 +1,13 @@
-export default function SectionTitle({ className, children }) {
+export default function SectionTitle({ className, children,id }) {
     return (
-      <h2 className={`${className}
+      <h2 
+        id={id}
+        className={`${className}
         mt-8 sm:mt-10 lg:mt-12
         text-xl sm:text-2xl lg:text-3xl 
         font-semibold text-white
-        text-center`
-        }>
+        text-center scroll-mt-14 sm:scroll-mt-14 lg:scroll-mt-20`}
+      >
         {children}
       </h2>
     );
